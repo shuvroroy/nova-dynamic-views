615157cb9c077b9b9b707d49ca47eabb0dcff7c3
Shuvro Roy <shuvro.nsu.cse@gmail.com>
2024-12-26 01:49:12 +0800

    chore: format

--- src/NovaDynamicViews.php
+++ src/NovaDynamicViews.php
@@ -1,20 +1,20 @@
 <?php
 
- namespace ShuvroRoy\NovaDynamicViews;
+namespace ShuvroRoy\NovaDynamicViews;
 
- use Laravel\Nova\Tool;
- use Illuminate\Http\Request;
+use Laravel\Nova\Tool;
+use Illuminate\Http\Request;
 use Laravel\Nova\Nova;
 
- class NovaDynamicViews extends Tool
- {
+class NovaDynamicViews extends Tool
+{
     public function boot(): void
     {
-        Nova::script('nova-dynamic-views', __DIR__.'/../dist/js/tool.js');
+        Nova::script('nova-dynamic-views', __DIR__ . '/../dist/js/tool.js');
     }
 
-     public function menu(Request $request): array
-     {
-         return [];
-     }
- }
+    public function menu(Request $request): array
+    {
+        return [];
+    }
+}
